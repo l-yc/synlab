c52a9b97c11a676b50c4c8f864a37e67c06cf77e
lyc <22719973+l-yc@users.noreply.github.com>
2020-04-12 12:03:07 +0800

    migrated to raphaeljs svg library

--- script.js
+++ script.js
@@ -13,68 +13,116 @@ let toolbar = null;
 
 const svgNS = 'http://www.w3.org/2000/svg';
 
-function makeAtom(text, pos) {
-    var node = document.createElementNS(svgNS, 'g');
-
-    var shape = document.createElementNS(svgNS, 'circle');
-    shape.setAttributeNS(null, 'cx', pos.x);
-    shape.setAttributeNS(null, 'cy', pos.y);
-    shape.setAttributeNS(null, 'r',  15);
-    shape.setAttributeNS(null, 'fill', 'white'); 
-    node.append(shape);
-
-    var newText = document.createElementNS(svgNS, 'text');
-    var textNode = document.createTextNode(text);
-    newText.appendChild(textNode);
-    newText.setAttributeNS(null, 'x', pos.x-7);
-    newText.setAttributeNS(null, 'y', pos.y+7);
-    node.append(newText);
-
-    node.classList.add('atom');
-
-    return node;
+function renderedTextSize(string, font, fontSize) {
+    var paper = Raphael(0, 0, 0, 0)
+    paper.canvas.style.visibility = 'hidden'
+    var el = paper.text(0, 0, string)
+    el.attr('font-family', font)
+    el.attr('font-size', fontSize)
+    var bBox = el.getBBox()
+    paper.remove()
+    return {
+        width: bBox.width,
+        height: bBox.height
+    }
 }
 
-function makeBond(pos) {
-    var node = document.createElementNS(svgNS, 'path');
+function trash() {
+    var color = "hsb(" + [(1 - R / max) * .5, 1, .75] + ")",
+        dt = r.circle(dx + 60 + R, dy + 10, R).attr({stroke: "none", fill: color});
+    if (R < 6) {
+        var bg = r.circle(dx + 60 + R, dy + 10, 6).attr({stroke: "none", fill: "#000", opacity: .4}).hide();
+    }
+    var lbl = r.text(dx + 60 + R, dy + 10, data[o])
+        .attr({"font": '10px "Helvetica Neue", Arial', stroke: "none", fill: "#fff"}).hide();
+    var dot = r.circle(dx + 60 + R, dy + 10, max).attr({stroke: "none", fill: "#000", opacity: 0});
+    dot.hover(function () {
+        if (bg) {
+            bg.show();
+        } else {
+            var clr = Raphael.rgb2hsb(color);
+            clr.b = .5;
+            dt.attr("fill", Raphael.hsb2rgb(clr).hex);
+        }
+        lbl.show();
+    }, function () {
+        if (bg) {
+            bg.hide();
+        } else {
+            dt.attr("fill", color);
+        }
+        lbl.hide();
+    });
+}
 
-    const len = 20;
-    const theta = Math.PI/6.0;
-    const dy = (len * Math.sin(theta)).toFixed(2);
-    const dx = (len * Math.cos(theta)).toFixed(2);
-    node.setAttributeNS(null, 'd',
+console.clear();
+
+function makeAtom(text, pos) {
+    console.log('making atom ' + text + ' at ' + JSON.stringify(pos));
+    const R = 20;
+
+    let dot = paper.circle(pos.x, pos.y, R)
+        .attr({ stroke: 'none', fill: '#dd7', opacity: .4 });
+    let lbl = paper.text(pos.x, pos.y , text)
+        .attr({'font': '14px "Helvetica Neue", Arial', stroke: '#000', fill: 'none'});
+    var atom = paper.set(dot, lbl);
+
+    atom.hover(event => {
+        dot.attr({ fill: '#dd7' });
+    }, event => {
+        dot.attr({ fill: '#fff' });
+    });
+
+    atom.click(event => {
+        makeBond(pos, event.ctrlKey ? 1 : 3);
+    });
+}
+
+function makeBond(pos, dirn) {
+    const len = 40;
+    let theta, dy, dx;
+    if (dirn == 1) {
+        theta = Math.PI/6.0;
+        dy = -(len * Math.sin(theta)).toFixed(2);
+        dx = (len * Math.cos(theta)).toFixed(2);
+    } else if (dirn == 3) {
+        theta = Math.PI/6.0;
+        dy = (len * Math.sin(theta)).toFixed(2);
+        dx = (len * Math.cos(theta)).toFixed(2);
+    }
+    
+    let bond = paper.path(
         `M ${pos.x} ${pos.y} l ${dx} ${dy}`
-    );
-    node.setAttributeNS(null, 'stroke-width', 2);
-    node.setAttributeNS(null, 'stroke-linecap', 'butt');
-    node.setAttributeNS(null, 'stroke', 'black');
-    node.setAttributeNS(null, 'x', pos.x);
-    node.setAttributeNS(null, 'y', pos.y);
+    ).attr({
+        'stroke-width': 2,
+        'stroke-linecap': 'butt',
+        'stroke': 'black',
+        'x': pos.x,
+        'y': pos.y,
+    });
 
-    node.classList.add('bond');
-    console.log(node);
-
-    return node;
+    makeAtom('C', { x: pos.x + parseFloat(dx), y: pos.y + parseFloat(dy) });
 }
 
 function initCanvas() {
     canvas = document.querySelector('#canvas');
+    paper = Raphael(canvas, 0, 0);
 
     canvas.addEventListener('click', event => {
         if (toolbar.active === null) return;
         console.log(event);
 
-
-        if (event.target === canvas) {
+        if (event.target === canvas.querySelector('svg')) {
             let pos = { x: event.offsetX, y: event.offsetY };
-            let atom = makeAtom(toolbar.active.dataset.value, pos);
-            canvas.appendChild(atom);
+            //let atom = makeAtom(toolbar.active.dataset.value, pos);
+            //canvas.appendChild(atom);
+            makeAtom(toolbar.active.dataset.value, pos);
         } else {
-            var rect = event.target.getBoundingClientRect();
-            console.log(rect);
-            let pos = { x: rect.x, y: rect.y };
-            let bond = makeBond(pos);
-            canvas.appendChild(bond);
+            //var rect = event.target.getBoundingClientRect();
+            //console.log(rect);
+            //let pos = { x: rect.x, y: rect.y };
+            //let bond = makeBond(pos);
+            //canvas.appendChild(bond);
         }
 
     }, false);
